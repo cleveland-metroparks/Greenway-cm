015704ac68acd28d1d07d3eade601134725aa156
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2019-08-01 15:28:27 -0400

    Delete ol.js

--- ol.js
+++
@@ -1,5 +0,0 @@
-var apiKey = 'pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiY2lsdWI2NndjMDE4cnZka3NpNXZwZnNiaSJ9.Pv2uRstAQcCq5EGTqK4o6A';
-
-olms('map', 'https://maps.tilehosting.com/styles/basic/style.json?key=' + apiKey).then(function(map) {
-  // do anything with the passed `ol.Map` instance here.
-});
