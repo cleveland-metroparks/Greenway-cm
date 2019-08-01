cc5a2b92138bdc913953821594d8984fff0cb777
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2019-08-01 14:45:12 -0400

    Add Mapbox API Key

--- ol.js
+++ ol.js
@@ -1,4 +1,4 @@
-var apiKey = 'insert_your_key_here';
+var apiKey = 'pk.eyJ1IjoiY2xldmVsYW5kLW1ldHJvcGFya3MiLCJhIjoiY2lsdWI2NndjMDE4cnZka3NpNXZwZnNiaSJ9.Pv2uRstAQcCq5EGTqK4o6A';
 
 olms('map', 'https://maps.tilehosting.com/styles/basic/style.json?key=' + apiKey).then(function(map) {
   // do anything with the passed `ol.Map` instance here.
