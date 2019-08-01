9c4084f4c3bbbf91484126c00d94b4983859be50
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2019-08-01 13:50:24 -0400

    Create ol.js
    This file will convert MapBox GL styles to OpenLayers?

---
+++ ol.js
@@ -0,0 +1,5 @@
+var apiKey = 'insert_your_key_here';
+
+olms('map', 'https://maps.tilehosting.com/styles/basic/style.json?key=' + apiKey).then(function(map) {
+  // do anything with the passed `ol.Map` instance here.
+});
